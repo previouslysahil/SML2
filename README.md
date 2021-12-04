6844edcf20d47a693938fb7ad33cc596f94d6ab3
Sahil Srivastava <previouslysahil@gmail.com>
2021-12-04 01:18:02 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SML2
+
+A description of this package.
---
+++ Tests/SML2Tests/SML2Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SML2
+
+final class SML2Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SML2().text, "Hello, World!")
+    }
+}
